593227126b213a6c7542262517848b9b78f121db
Jay Mehta <23277339+jaymehta20@users.noreply.github.com>
2024-05-09 15:39:34 +0530

    start with resend

--- medusa-config.js
+++ medusa-config.js
@@ -72,6 +72,31 @@ const plugins = [
       return_action: "create_fulfillment", //'create_fulfillment' or 'create_order'(default) (required)
     },
   },
+  {
+    resolve: `medusa-plugin-resend`,
+    options: {
+      api_key: process.env.RESEND_API_ID,
+      from: "jay.mehta@intelliconnectq.com",
+      enable_endpoint: process.env.SES_ENABLE_ENDPOINT,
+      template_path: "./src/email-templates/order-created.tsx",
+      subject_template_type: "text",
+      body_template_type: "react",
+      order_placed_template: "order_placed",
+      order_shipped_template: "order_shipped",
+      customer_password_reset_template: "customer_password_reset",
+      gift_card_created_template: "gift_card_created",
+      //order_canceled_template: 'order_canceled',
+      //order_refund_created_template: 'order_refund_created',
+      //order_return_requested_template: 'order_return_requested',
+      //order_items_returned_template: 'order_items_returned',
+      //swap_created_template: 'swap_created',
+      //swap_shipment_created_template: 'swap_shipment_created',
+      //swap_received_template: 'swap_received',
+      //claim_shipment_created_template: 'claim_shipment_created',
+      //user_password_reset_template: 'user_password_reset',
+      //medusa_restock_template: 'medusa_restock',
+    },
+  },
 ];
 
 const modules = {
