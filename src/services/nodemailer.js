752b0bda07591bbc0b4ab78d41c335cf1edbaa5e
Anil_barry <annenex.11@gmail.com>
2024-05-13 15:30:35 +0530

    email template

--- src/services/nodemailer.js
+++ src/services/nodemailer.js
@@ -34,24 +34,24 @@ class NodemailerService extends NotificationService {
                 // text: `Order with id ${eventData.id} has been placed.`,
                 html: `
                 <div style="font-family: Arial, sans-serif; color: #333333; line-height: 1.5;">
-                <h2 style="color: #0077b6;">Hey ,</h2>
+                <h2 style="color: #0077b6;">Greeting,</h2>
                 <p>Thank you for your inquiry. Our team will reach out to you shortly.</p>
-                <a href="https://www.shopntrolly.com/in/account/orders" style="display: inline-block; background-color: #0077b6; color: #ffffff; padding: 10px 20px; text-decoration: none; border-radius: 4px;">VIEW OR MANAGE ORDER</a>
+                <a href="https://www.shopntrolly.com/in/account/" style="display: inline-block; background-color: #0077b6; color: #ffffff; padding: 10px 20px; text-decoration: none; border-radius: 4px;">VIEW OR MANAGE ORDER</a>
             
                 <p><strong>Your Request Id ${eventData.id} has been placed.</strong></p>
                 <p>You can make your shopping experience easier, quicker and more personal - access your order history, get recommended products, exclusive offers, wishlist collections and more simply by creating an exclusive account.</p>
                 <p>Say hello to us! Drop in an email on <a href="mailto:hello@mail.shotntrolly.com" style="color: #0077b6;">hello@mail.shotntrolly.com</a> or call us on [+91 9156834305 / +91 8007529003] (We're available from 9 am to 5 pm on all days).</p>
                 <p>Thanks,<br>ShopNtrolly</p>
                 <div style="display: flex; align-items: center; margin-top: 20px;">
-                Connect with us
+                Connect with us {" "}
                 <a
-                  href="https://www.instagram.com/shopandtrolly/"
+                  href=${"https://www.instagram.com/shopandtrolly/"}
                   target="_blank"
                   rel="ShopNTrolly website"
-                >
-                
+                > 
+                Instagram {" "}
                 </a>
-                &
+                & {" "}
                 <a
                   href=${"https://www.youtube.com/@shopntrolly"}
                   target="_blank"
