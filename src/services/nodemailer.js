a47c3a927a48cecdd4efc83e2d4ab9138a5344ce
Anil_barry <annenex.11@gmail.com>
2024-05-13 15:18:05 +0530

    testing email template

--- src/services/nodemailer.js
+++ src/services/nodemailer.js
@@ -26,6 +26,7 @@ class NodemailerService extends NotificationService {
         console.log(eventData, "event DAta")
         if (eventName === 'order.placed') {
             const order = await this.orderService.retrieve(eventData.id);
+            console.log(order, "order details")
             const mailOptions = {
                 from: process.env.MAIL_USER,
                 to: order.email, // replace with the recipient's email
@@ -39,7 +40,7 @@ class NodemailerService extends NotificationService {
             
                 <p><strong>Your Request Id ${eventData.id} has been placed.</strong></p>
                 <p>You can make your shopping experience easier, quicker and more personal - access your order history, get recommended products, exclusive offers, wishlist collections and more simply by creating an exclusive account.</p>
-                <p>Say hello to us! Drop in an email on <a href="mailto:hello@mail.shotntrolly.com" style="color: #0077b6;">hello@mail.shotntrolly.com</a> or call us on [] (We're available from 9 am to 5 pm on all days).</p>
+                <p>Say hello to us! Drop in an email on <a href="mailto:hello@mail.shotntrolly.com" style="color: #0077b6;">hello@mail.shotntrolly.com</a> or call us on [+91 9156834305 / +91 8007529003] (We're available from 9 am to 5 pm on all days).</p>
                 <p>Thanks,<br>ShopNtrolly</p>
                 <div style="display: flex; align-items: center; margin-top: 20px;">
                 Connect with us
@@ -52,11 +53,11 @@ class NodemailerService extends NotificationService {
                 </a>
                 &
                 <a
-                  href="https://www.youtube.com/@shopntrolly"
+                  href=${"https://www.youtube.com/@shopntrolly"}
                   target="_blank"
                   rel="ShopNTrolly website"
                 >
-                    
+                    Youtube
                 </a>
                 </div>
               </div>
@@ -83,15 +84,32 @@ class NodemailerService extends NotificationService {
                 subject: 'Order Placed',
                 html: `
                 <div style="font-family: Arial, sans-serif; color: #333333; line-height: 1.5;">
-                <h2 style="color: #0077b6;">Hey [First Name],</h2>
+                <h2 style="color: #0077b6;">Hey ,</h2>
                 <p>Thank you for your inquiry. Our team will reach out to you shortly.</p>
-                <a href="#" style="display: inline-block; background-color: #0077b6; color: #ffffff; padding: 10px 20px; text-decoration: none; border-radius: 4px;">VIEW OR MANAGE ORDER</a>
-                <p><strong>Your Request #725253480</strong></p>
-                <p><strong>Order with id ${eventData.id} has been placed.</strong></p>
+                <a href="https://www.shopntrolly.com/in/account/orders" style="display: inline-block; background-color: #0077b6; color: #ffffff; padding: 10px 20px; text-decoration: none; border-radius: 4px;">VIEW OR MANAGE ORDER</a>
+            
+                <p><strong>Your Request Id ${eventData.id} has been placed.</strong></p>
                 <p>You can make your shopping experience easier, quicker and more personal - access your order history, get recommended products, exclusive offers, wishlist collections and more simply by creating an exclusive account.</p>
-                <p>Say hello to us! Drop in an email on <a href="mailto:hello@mail.shotntrolly.com" style="color: #0077b6;">hello@mail.shotntrolly.com</a> or call us on [] (We're available from 9 am to 5 pm on all days).</p>
+                <p>Say hello to us! Drop in an email on <a href="mailto:hello@mail.shotntrolly.com" style="color: #0077b6;">hello@mail.shotntrolly.com</a> or call us on [+91 9156834305 / +91 8007529003] (We're available from 9 am to 5 pm on all days).</p>
                 <p>Thanks,<br>ShopNtrolly</p>
-                <p>Social Media Logos and URL</p>
+                <div style="display: flex; align-items: center; margin-top: 20px;">
+                Connect with us
+                <a
+                  href="https://www.instagram.com/shopandtrolly/"
+                  target="_blank"
+                  rel="ShopNTrolly website"
+                >
+                
+                </a>
+                &
+                <a
+                  href="https://www.youtube.com/@shopntrolly"
+                  target="_blank"
+                  rel="ShopNTrolly website"
+                >
+                    
+                </a>
+                </div>
               </div>
               `
             };
