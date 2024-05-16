0eab0a054aebefc551397f1654a420c4b0fb65f0
Anil_barry <annenex.11@gmail.com>
2024-05-16 15:42:57 +0530

    agolia search

--- medusa-config.js
+++ medusa-config.js
@@ -84,6 +84,40 @@ const plugins = [
                 webhook_secret: process.env.RAZORPAY_SECRET,
     }
   },
+  const plugins = [
+    // ...
+    {
+      resolve: `medusa-plugin-algolia`,
+      options: {
+        applicationId: process.env.ALGOLIA_APP_ID,
+        adminApiKey: process.env.ALGOLIA_ADMIN_API_KEY,
+        settings: {
+          products: {
+            indexSettings: {
+              searchableAttributes: ["title", "description"],
+              attributesToRetrieve: [
+                "id",
+                "title",
+                "description",
+                "handle",
+                "thumbnail",
+                "variants",
+                "variant_sku",
+                "options",
+                "collection_title",
+                "collection_handle",
+                "images",
+              ],
+            },
+            transformer: (product) => ({ 
+              id: product.id, 
+              // other attributes...
+            }),
+          },
+        },
+      },
+    },
+  ]
 ];
 
 const modules = {
